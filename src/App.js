0deb44e13961048b11e371c976cbddfb855505cd
Satpal <jassal.spj@gmail.com>
2024-02-15 17:30:26 +0530

    final changes

--- src/App.js
+++ src/App.js
@@ -6,38 +6,71 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Budget from './components/Budget';
 
 // Add code to import the other components here under
-
+import Remaining from './components/Remaining';
+import ExpenseTotal from './components/ExpenseTotal';
 
 import { AppProvider } from './context/AppContext';
+import ExpenseList from './components/ExpenseList';
+
+import AllocationForm from './components/AllocationForm';
+import CurrencyChange from './components/CurrencyChange';
 const App = () => {
     return (
         <AppProvider>
             <div className='container'>
                 <h1 className='mt-3'>Company's Budget Allocation</h1>
-                    <div className='row mt-3'>
-                        {
+                 
+                <div className='row mt-3'>
+                    {
                             /* Add Budget component here */
-                        }        
+                            <div className='col-sm'>
+                               <CurrencyChange /> 
+                            </div>    
+                    }  
+                    </div>
+                    <div className='row mt-3'>
+                    {
+                            /* Add Budget component here */
+                            <div className='col-sm'>
+                                <Budget />
+                            </div>    
+                    }        
 
-                        {
+                    {
                             /* Add Remaining component here*/
-                        }        
+                            <div className='col-sm'>
+                                <Remaining />
+                            </div>
+                    }        
 
-                        {
+                    {
                             /* Add ExpenseTotal component here */
-                        }        
-                       
-                        {
+                            <div className='col-sm'>
+                                <ExpenseTotal />
+                            </div>
+                    } 
+                         
+                    </div>       
+                    <div className='row mt-3'> 
+                    {
                             /* Add ExpenseList component here */
+                            <div className='col-sm'>
+                                <ExpenseList />
+                            </div>
                         }         
 
-                        {
+                    {
                             /* Add ExpenseItem component here */
-                        }        
-
-                        {
+                            
+                    }        
+                    </div>
+                    <div className='row mt-3'>
+                    {
                             /* Add AllocationForm component here under */
-                        }        
+                            <div className='col-sm'>
+                             <AllocationForm />
+                            </div>
+                    }        
 
                 </div>
             </div>
---
+++ src/components/CurrencyChange.js
@@ -0,0 +1,37 @@
+import React, { useState,useContext,useEffect } from 'react';
+import { AppContext } from '../context/AppContext';
+
+const CurrencyChange = () => {
+    const [currency, setCurrency] = useState('');
+    const { dispatch } = useContext(AppContext);
+
+const handleChange = (event) => {
+    setCurrency(event.target.value);
+    changeCurrency()
+  };
+
+  const changeCurrency = () => {
+    dispatch({
+        type: 'CHG_CURRENCY',
+        payload: currency,
+    });
+  }
+
+useEffect(() => {console.log(currency);}, [currency] )
+
+return (
+    <div className='alert alert-primary' style={{paddingRight:'10px'}}>
+        <label>
+            Change Currency:
+        </label>
+        <select id="inputGroupSelect02" onChange={handleChange}>
+            <option defaultValue>Currency</option>
+            <option value="$" name="dollar">$ Dollar</option>
+            <option value="£" name="pound">£ Pound</option>
+            <option value="€" name="euro">€ Euro</option>
+            <option value="₹" name="ruppee">₹ Ruppee</option>
+        </select>
+    </div>
+)}
+
+export default CurrencyChange;
